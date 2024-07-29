422dfe5d69e06deed04d05165435df420a64fd2b
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-07-29 07:09:56 +0300

    delete the plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,21 +133,6 @@ module.exports = {
           },
         ],
       },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Oswald`,
-            subsets: [`latin`],
-          },
-          {
-            family: `Open Sans`,
-            variants: [`400`, `700`]
-          },
-        ],
-      },
     }
   ],
 }
