9a1c971bb14d4e22e03b1c304229b11a5ad4b2cc
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-07-29 11:56:30 +0300

    tags page customization

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,7 +2,21 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Bio from "../components/bio"
+import { Helmet } from "react-helmet"
 
+class Application extends React.Component {
+  render() {
+    return (
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>الاقسام | مدونة شناوي</title>
+          <link rel="canonical" href="/" />
+        </Helmet>
+      </div>
+    )
+  }
+}
 const TagsPage = ({ data , location}) => {
   const tags = data.allMarkdownRemark.group
   const siteTitle = data.site.siteMetadata?.title || `الاقسام`
