e7321e30bfe41d2c4d115e26e3c2f371b89e0459
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-07-29 07:07:00 +0300

    gatsby-plugin-prefetch-google-fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,5 +134,20 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    }
   ],
 }
