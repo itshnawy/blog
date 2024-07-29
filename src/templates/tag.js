1859ecf13216a1d94e81c8ac01f3c4bc0cd5987a
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-07-29 12:07:00 +0300

    tag page

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import Layout from "../components/layout"
+import Bio from "../components/bio"
+import Seo from "../components/seo"
 
 const TagPage = ({ data, pageContext }) => {
   const { tag } = pageContext
   const posts = data.allMarkdownRemark.edges
-
+  
   return (
-    <div>
-      <h1>Posts tagged with "{tag}"</h1>
+    <div id="tagswrapper">
+               <Layout location='/' title={siteTitle}>
+               <Bio /> 
+      <h3>التدوينات التابعه للقسم "{tag}"</h3>
       <ul>
         {posts.map(({ node }) => (
           <li key={node.fields.slug}>
@@ -15,12 +20,18 @@ const TagPage = ({ data, pageContext }) => {
           </li>
         ))}
       </ul>
+      </Layout>
     </div>
   )
 }
-
+export const Head = () => <Seo title={tag} />
 export const pageQuery = graphql`
   query($tag: String) {
+       site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
       limit: 1000
       sort: { frontmatter: { date: DESC } }
